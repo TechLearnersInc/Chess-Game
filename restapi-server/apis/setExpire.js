b8f2e1a0291a22113d309db0a99bc59b59604b08
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2021-07-26 04:07:52 +0600

    updated

--- restapi-server/apis/setExpire.js
+++ restapi-server/apis/setExpire.js
@@ -5,7 +5,9 @@ async function setExpire(req, res, next) {
   const data = req.body;
 
   try {
-    await redis.expire(data.gamecode, data.expiresIn);
+    const status = await redis.expire(data.gamecode, data.expiresIn);
+    // 0 => Failed and 1 => Success
+    if (status === 0) throw new Error("Gamecode doesn't exist");
   } catch (err) {
     console.error(err);
     next(new restify_err.InternalServerError(err.message));
