4bc259979ffe18ac2125398ded059b1caa5243be
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2021-07-27 01:11:54 +0600

    updated

--- socket-server/config/rest-client-endpoints.js
+++ socket-server/config/rest-client-endpoints.js
@@ -2,4 +2,11 @@ module.exports = {
   new_gamecode: '/create-gamecode',
   set_expire: '/set-expire',
   get_gamedata: '/get-gamedata',
+  set_player_lefted: '/set-player-lefted',
+  set_player_joined: '/set-player-joined',
+  set_player_turn: '/set-player-turn',
+  set_fen: '/set-fen',
+  get_player_join_details: '/get-player-join-details',
+  get_current_turn: '/get-current-turn',
+  check_if_this_player_turn: '/check-if-this-player-turn',
 };
